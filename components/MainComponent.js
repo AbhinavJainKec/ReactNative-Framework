1179e0dbfa5ad919a16a529167c42050427f9509
Abhinav Jain <abhinav.jain@krishnacollege.ac.in>
2020-06-26 04:04:51 +0530

    Navigation 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
+import Home from './HomeComponent';
 import Dishdetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 
 const MenuNavigator = createStackNavigator({
   Menu: { screen: Menu },
@@ -21,10 +23,44 @@ const MenuNavigator = createStackNavigator({
         color: "#fff"            
     }
   }
-}
-);
+});
 
-const MenuNavigatorContainer = createAppContainer(MenuNavigator);
+const HomeNavigator = createStackNavigator({
+  Home: { screen: Home }
+}, 
+{
+  navigationOptions: {
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+        color: "#fff"            
+    }
+  }
+});
+
+const MainNavigator = createDrawerNavigator ({
+  Home: 
+    { screen: HomeNavigator,
+      navigationOptions: {
+        title: 'Home',
+        drawerLabel: 'Home'
+      }
+    },
+  Menu: 
+    { screen: MenuNavigator,
+      navigationOptions: {
+        title: 'Menu',
+        drawerLabel: 'Menu'
+      }, 
+    }
+}, 
+{
+drawerBackgroundColor: '#D1C4E9'
+});
+
+const MainNavigatorContainer = createAppContainer(MainNavigator);
 
 class Main extends Component {
 
@@ -32,7 +68,7 @@ class Main extends Component {
  
     return (
       <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-        <MenuNavigatorContainer />
+        <MainNavigatorContainer />
       </View>
     );
   }
