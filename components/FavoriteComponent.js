89fa1dd96074a52b42ae2ec169f89267de537c94
Abhinav Jain <abhinav.jain@krishnacollege.ac.in>
2020-07-11 02:35:01 +0530

    Alert

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
@@ -27,16 +27,26 @@ class Favorites extends Component {
     render() {
 
         const { navigate } = this.props.navigation;
-
-        const rightButton = [
-            {
-                text: 'Delete', 
-                type: 'delete',
-                onPress: () => this.props.deleteFavorite(item.id)
-            }
-        ];
         
         const renderMenuItem = ({item, index}) => {
+
+            const rightButton = [
+                {
+                    text: 'Delete', 
+                    type: 'delete',
+                    onPress: () => {
+                        Alert.alert(
+                            'Delete Favorite ?',
+                            'Are you sure you wish to delete the favorite dish ' + item.name + '?',
+                            [
+                                { text: 'Cancel', onPress: () => console.log(item.name + ' Not Deleted...'), style: 'cancel' },
+                                { text: 'OK', onPress: () => this.props.deleteFavorite(item.id) }
+                            ],
+                            { cancelable: false }
+                        );
+                    }
+                }
+            ];
     
             return (
                 <Swipeout right={rightButton} autoClose={true} >
